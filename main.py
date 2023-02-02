9e8df7a18fe4390a311f8e3be29c3b18aaeda698
Tamás Börcsök <tborcsok@gmail.com>
2023-02-02 16:20:25 +0100

    add logging to file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -6,12 +8,21 @@ load_dotenv()
 
 from src.cli import cli
 
+root = Path(__file__).parent / "logs"
+
 if __name__ == "__main__":
+    root.mkdir(exist_ok=True)
+
+    started = datetime.utcnow()
 
     logging.basicConfig(
         format="%(levelname)s:%(asctime)s:%(message)s",
         level=logging.INFO,
         datefmt="%Y-%m-%d %H:%M:%S%z",
+        handlers=[
+            logging.FileHandler(root / f"{started.date().isoformat()}.log"),
+            logging.StreamHandler(),
+        ],
     )
 
     cli()
