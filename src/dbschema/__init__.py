f0737422d6d937dea0fcab25dc14a6da354cf6cc
Tamás Börcsök <tborcsok@gmail.com>
2023-02-02 16:26:21 +0100

    explicitly define path to sqlite file

--- src/dbschema/__init__.py
+++ src/dbschema/__init__.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 
-from src.setup import DB_FILE
+from src.setup import DB_PATH
 
-engine = create_engine(f"sqlite:///{DB_FILE}", echo=False)
+engine = create_engine(f"sqlite:///{DB_PATH.absolute()}", echo=False)
--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,9 @@
 import os
 from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).parents[1]
+
 LASTFM_API_USER = os.environ["LASTFM_API_USER"]
 LASTFM_API_KEY = os.environ["LASTFM_API_KEY"]
 
-DB_FILE = "lastfm.db"
-DB_PATH = Path(DB_FILE)
+DB_PATH = PROJECT_ROOT / "lastfm.db"
